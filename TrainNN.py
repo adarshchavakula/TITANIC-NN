a54934a63095b496835c24040d16a0d4e6ac3148
Adarsh Chavakula <adarsh.chavakula@gmail.com>
2015-03-02 00:35:42 +0530

    Update TrainNN.py
    New - A train/CV split for cross validation. Different threshold values analyzed for 0-1 cutoff. A new data feature based on Cabin ticket. Increased number of nodes in hidden layers.
    Next Version - Have a log-loss function instead of mse. More feature extraction. More nodes in hidden layes

--- TrainNN.py
+++ TrainNN.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import minimize
+from sklearn.cross_validation import train_test_split
 import pandas as pd
 
 
@@ -28,6 +29,12 @@ def MakeFeatures(Raw):
 	fares = np.array(Raw['Fare'])
 	norm = (np.divide(np.ones([N,]),1+np.exp(-0.01*fares))-0.5)*2	
 	features['Fare'] = norm
+	# get cabin info
+	#cabin = np.array(map(str,np.array(Raw['Cabin'])))
+	#iscabin = np.array(map(int,cabin!='nan'))
+	#features['IsCabin'] = iscabin
+	#print features
+
 
 	return features
 
@@ -74,9 +81,9 @@ def TrainNN(train,response,hidden = [10,10]):
 	nw1 = (S+1)*h1
 	nw2 = (h1+1)*h2
 	nw3 = (h2+1)
-	
+	np.random.seed(42)
 	init_weights = np.random.randn(nw1+nw2+nw3)
-	res = minimize(err, init_weights, args = (train, response, hidden), options = {'maxiter':500})
+	res = minimize(err, init_weights, args = (train, response, hidden), options = {'maxiter':1000})
 	return res.x
 
 
@@ -87,19 +94,45 @@ def main():
 	features = MakeFeatures(train)
 	response = np.array(train['Survived'])
 
+	tr, cv, y_tr, y_cv = train_test_split(features,response, test_size=0.20, random_state=123)
+
 	print 'Training the Neural Network...'
-	NNshape = [10,10]
-	best_weights = TrainNN(np.array(features),response,hidden = NNshape)
+	NNshape = [20,20]
+	best_weights = TrainNN(np.array(tr),y_tr,hidden = NNshape)
 	'''
 	print '\n\nEstimated best weights are:'
 	print best_weights
 	'''
-	preds = NNFwdProp(features, best_weights, NNshape)
-	#print '\n\nTrain Predictions:'
-	#print map(int,np.round(preds))
+	
+	
+	for thresh in 0.01*np.array(range(30,85,5)):
+		tr_preds = NNFwdProp(tr, best_weights, NNshape)
+		cv_preds = NNFwdProp(cv, best_weights, NNshape)
+		tr_preds[tr_preds>=thresh]=1
+		tr_preds[tr_preds<thresh]=0
+		cv_preds[cv_preds>=thresh]=1
+		cv_preds[cv_preds<thresh]=0
+		print "Threshold = "+ str(thresh)
+		print "Train Accuracy = "+ str(100*(1-np.sum(np.abs(tr_preds-y_tr))/len(y_tr)))
+		print "CV Accuracy = "+ str(100*(1-np.sum(np.abs(cv_preds-y_cv))/len(y_cv)))
+	
 
-	print "Train Accuracy = "+ str(100*(1-np.sum(np.abs(preds-response))/len(features)))
-
+	print 'Reading test data...'
+	test = pd.read_csv('test.csv')
+	print 'Extracting features...'
+	x_test = MakeFeatures(test)
+	print 'Making predictions for test data...'
+	preds_test = NNFwdProp(x_test, best_weights, NNshape)
+	preds_test[preds_test>=0.6]=1
+	preds_test[preds_test<0.6]=0
+	preds_test[np.isnan(preds_test)]=0
+	preds_test = map(int, preds_test)
+	
+	print 'Writing to CSV file..'
+	template = pd.read_csv('genderclassmodel.csv')
+	template['Survived'] = preds_test
+	template.to_csv('Test_predictions.csv', index = False)
+	print 'Done!!'
 
 	return
 
